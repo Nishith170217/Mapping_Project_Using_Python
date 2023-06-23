aebe400c5b0680c41ae1d7b6f9ee456f40672e30
Nishith170217 <nishith170217@gmail.com>
2023-06-23 21:11:58 +0200

    US volcano location update

--- map1.py
+++ map1.py
@@ -1,8 +1,18 @@
 import folium
-map = folium.Map(location=[23.828296, 90.405122],zoom_start=7)
+import pandas as pd
+
+data = pd.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+#print(lat)
+map = folium.Map(location=[40.393106, -113.110520],zoom_start=4)
 fg= folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[23.828296, 90.405122], popup="Airport", icon=folium.Icon(color='red')))
-fg.add_child(folium.Marker(location=[22.780399, 89.876453], popup="My Home", icon=folium.Icon(color='green')))
+
+for lt,ln,el in zip(lat,lon,elev):
+#fg.add_child(folium.Marker(location=[23.828296, 90.405122], popup="Airport", icon=folium.Icon(color='red')))
+    fg.add_child(folium.Marker(location=[lt,ln], popup=str(el) +" m", icon=folium.Icon(color='green')))
 #22.780399, 89.876453
+
 map.add_child(fg)
 map.save("Map1.html")
