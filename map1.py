fb6aeff54da3e3a412c1d2aa9997191f0908fa97
Nishith170217 <nishith170217@gmail.com>
2023-06-24 22:50:32 +0200

    added multi-color in marker

--- map1.py
+++ map1.py
@@ -5,13 +5,22 @@ data = pd.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
+
+#colored marker
+def color_producers(elev):
+    if elev<1500:
+        return "green"
+    elif 1500<=elev and elev<2400:
+        return "orange"
+    else:
+        return "red"
 #print(lat)
 map = folium.Map(location=[40.393106, -113.110520],zoom_start=4)
 fg= folium.FeatureGroup(name="My Map")
 
 for lt,ln,el in zip(lat,lon,elev):
 #fg.add_child(folium.Marker(location=[23.828296, 90.405122], popup="Airport", icon=folium.Icon(color='red')))
-    fg.add_child(folium.Marker(location=[lt,ln], popup=str(el) +" m", icon=folium.Icon(color='green')))
+    fg.add_child(folium.Marker(location=[lt,ln], popup=str(el) +" m", icon=folium.Icon(color=color_producers(el))))
 #22.780399, 89.876453
 
 map.add_child(fg)
