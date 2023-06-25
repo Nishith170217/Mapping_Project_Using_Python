376bc8d98acb60e387491cf4b3ebd36e95df01ba
Nishith170217 <nishith170217@gmail.com>
2023-06-25 19:04:49 +0200

    adding color according to population

--- map1.py
+++ map1.py
@@ -22,7 +22,8 @@ for lt,ln,el in zip(lat,lon,elev):
 #fg.add_child(folium.Marker(location=[23.828296, 90.405122], popup="Airport", icon=folium.Icon(color='red')))
     fg.add_child(folium.CircleMarker(location=[lt,ln], radius= 6, popup=str(el) +" m", fill_color=color_producers(el), color = "grey", fill_opacity = 0.8))
 #22.780399, 89.876453
-fg.add_child(folium.GeoJson(data=(open('world.json','r',encoding='utf-8-sig').read())))
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(), style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005']< 10000000 
+else 'orange' if 10000000<= x['properties']['POP2005']< 20000000 else 'red'}))
 
 map.add_child(fg)
 map.save("Map1.html")
